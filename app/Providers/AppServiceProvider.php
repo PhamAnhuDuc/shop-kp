e7f801cca5dd59cbe589216f624228013914e377
phamanhduc <phamanhduc536@gmail.com>
2018-09-11 01:54:53 +0700

    ud

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,10 +15,32 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    // public function boot()
+    // {
+    //     // Schema:defaultStringLength(191);
+    //     //share - view chia sẻ //truyền thông tin của giỏ hàng đi và in ra sản phẩm
+    //     view()->composer('header', function ($view) {
+    //         $a_count = (Cart::count());
+    //         $b_price = (Cart::total());
+    //         $loai_sp = ProductType::all();
+    //         $view->with(['loaisanpham'=>$loai_sp,'a_count'=>$a_count,'b_price'=>$b_price]);
+    //     });
+    // }
+    // /**
+    //  * Register any application services.
+    //  *
+    //  * @return void
+    //  */
+    // public function register()
+    // {
+    //     //
+    // }
+    public function boot(UrlGenerator $url)
     {
-        // Schema:defaultStringLength(191);
-        //share - view chia sẻ //truyền thông tin của giỏ hàng đi và in ra sản phẩm
+        if(env('REDIRECT_HTTPS')) {
+            $url->formatScheme('https');
+        }
+        
         view()->composer('header', function ($view) {
             $a_count = (Cart::count());
             $b_price = (Cart::total());
@@ -26,6 +48,7 @@ class AppServiceProvider extends ServiceProvider
             $view->with(['loaisanpham'=>$loai_sp,'a_count'=>$a_count,'b_price'=>$b_price]);
         });
     }
+ 
     /**
      * Register any application services.
      *
@@ -33,6 +56,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        if(env('REDIRECT_HTTPS')) {
+            $this->app['request']->server->set('HTTPS', true);
+        }
     }
 }
